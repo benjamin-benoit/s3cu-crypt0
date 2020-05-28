ba16c2a79161773c6ec5e140924812a5b5533875
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2020-05-28 15:38:41 +0200

    exo6 not yet

--- src/exercices/ex06.challengeaccepted.ts
+++ src/exercices/ex06.challengeaccepted.ts
@@ -1,1 +1,31 @@
-export default function challengeaccepted() {}
+export default function challengeAccepted(
+    message: string = 'Yo Majdi ',
+    key: string = 'Mhirba',
+    isDecode: 0 | 1 = 0,
+) {
+    if (isDecode === 0) {
+        const textToChars = (text: string) =>
+            text.split('').map(c => c.charCodeAt(0));
+        const byteHex = (n: any) => ('0' + Number(n).toString(16)).substr(-2);
+        const applySaltToChar = (code: any) =>
+            textToChars(key).reduce((a, b) => a ^ b, code);
+
+        return message
+            .split('')
+            .map(textToChars)
+            .map(applySaltToChar)
+            .map(byteHex)
+            .join('');
+    } else {
+        const textToChars = (text: string) =>
+            text.split('').map(c => c.charCodeAt(0));
+        const applySaltToChar = (code: any) =>
+            textToChars(key).reduce((a, b) => a ^ b, code);
+        return (message as any)
+            .match(/.{1,2}/g)
+            .map((hex: string) => parseInt(hex, 16))
+            .map(applySaltToChar)
+            .map((charCode: number) => String.fromCharCode(charCode))
+            .join('');
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { ex01, ex02, ex03, ex04, ex05 } from './exercices';
+import { ex01, ex02, ex03, ex04, ex05, ex06 } from './exercices';
 
 // console.log(ex01());
 // console.log(ex02());
 // console.log(ex03());
 // console.log(ex04());
-console.log(ex05());
+// console.log(ex05());
+// encode
+console.log(ex06('12 23 43', 'Mhirba', 1));
+// decode
+console.log(ex06('0c0f1d0f0e1d090e', 'Mhirba', 1));
