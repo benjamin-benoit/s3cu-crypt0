fdee4a1617058e8bf09a1fd58c7fc3169f898f31
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2020-05-28 10:33:31 +0200

    fix ex01

--- src/exercices/01_Convert_hex_to_base64.ts
+++
@@ -1,3 +0,0 @@
-export default function convert_hex_to_base64(str: string = '10101') {
-    return str;
-}
--- src/exercices/index.ts
+++ src/exercices/index.ts
@@ -1,1 +1,6 @@
-export { default as exo01 } from './01_Convert_hex_to_base64';
+export { default as exo01 } from './ex01.hex2base64';
+export { default as exo02 } from './ex02.fixedxor';
+export { default as exo03 } from './ex03.singlebytexor';
+export { default as exo04 } from './ex04.xordetect';
+export { default as exo05 } from './ex05.reapeatxor';
+export { default as exo06 } from './ex06.challengeaccepted';
